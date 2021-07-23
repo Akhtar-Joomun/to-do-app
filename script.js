457da2efc8452a4c59f4e8af1a4eaa9b36103e5f
Ak0706 <sakhtar7681@gmail.com>
2021-07-23 18:08:30 +0400

    Fix linter errors

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let form = document.querySelector('.form');
-let myInput = document.querySelector('.my-input');
-let myUl = document.querySelector('.my-ul');
+const form = document.querySelector('.form');
+const myInput = document.querySelector('.my-input');
+const myUl = document.querySelector('.my-ul');
 
-
+const createList = (x) => {
+  const myHtml = `<div class="my-lists">
+      <li class="my-list">${x}</li>
+      <button onclick="deleteItem(this)" class="del-button">Delete</button>
+  </div>`;
+  myUl.insertAdjacentHTML('beforeend', myHtml);
+};
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    createList(myInput.value);
-    myInput.value = '';
+  e.preventDefault();
+  createList(myInput.value);
+  myInput.value = '';
 });
 
-let createList = (x) => {
-    let myHtml = `<div class="my-lists">
-    <li class="my-list">${x}</li>
-    <button onclick="deleteItem(this)" class="del-button">Delete</button>
-</div>`
-    myUl.insertAdjacentHTML('beforeend', myHtml);
-}
-
-let deleteItem = (x) => {
-    x.parentElement.remove();  
-}
+// eslint-disable-next-line
+const deleteItem = (x) => {
+  x.parentElement.remove();
+};
